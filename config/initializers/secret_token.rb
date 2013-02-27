8efc9924af232b9f81e77fd1c4a2eadcb1f10681
Sheng-Loong Su <sushengloong@gmail.com>
2013-02-27 21:08:24 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyReports::Application.config.secret_token = '82904db5f1cf6b97ae591f4212bd796df4f79c9a099375a07eaa8153369f0722eb57aa539828d2592a21eea0cf3fbc6da8ec67aebc36c6736c261f8b40fc1b97'
